07003f902d5a9e09c34b965d284433d9fe3720d9
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-09-26 14:43:15 +0800

    Adding histogram functionality. ATP

---
+++ src/histogram.rs
@@ -0,0 +1,86 @@
+use statrs::statistics::Data;
+use statrs::statistics::OrderStatistics;
+
+fn arange(start: f64, end: f64, step: f64) -> Vec<f64> {
+    let mut range = Vec::new();
+    let mut location = start;
+
+    while location < end {
+        range.push(location);
+        location += step;
+    }
+    range
+}
+
+pub fn histogram(data: Vec<f64>, bins: Vec<f64>) -> Vec<usize> {
+    let mut counts = vec![0; bins.len() - 1];
+
+    for x in data {
+        // find the bin index
+        if let Some(idx) = bins.windows(2).position(|w| x >= w[0] && x < w[1]) {
+            counts[idx] += 1;
+        } else if x == *bins.last().unwrap() {
+            // include right edge like NumPy does
+            counts[bins.len() - 2] += 1;
+        }
+    }
+
+    counts
+}
+
+/// Calculates the Freedman–Diaconis_rule for binwidth.
+fn calculate_fd(data: Vec<f64>) -> f64 {
+    let mut x = Data::new(data.clone());
+    let iqr = x.upper_quartile() - x.lower_quartile();
+    let n = data.clone().len() as f64;
+    2. * (iqr / n.powf(1. / 3.))
+}
+
+/// Histogram that automatically selects the "optimal" bins using the Freedman-Diaconis rule
+pub fn histogram_fd(data: Vec<f64>, end: f64) -> Vec<usize> {
+    let bin_width = calculate_fd(data.clone());
+    let bins = arange(0., end, bin_width);
+    histogram(data, bins)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::iter::zip;
+
+    #[test]
+    fn test_histogram() {
+        // testing against numpy histogram
+        let data = arange(0., 100., 1.);
+        let bins = arange(10., 20., 0.5);
+        let result = histogram(data, bins);
+        let answer = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1];
+        for (r, a) in zip(result, answer) {
+            assert_eq!(r, a as usize);
+        }
+    }
+
+    #[test]
+    fn test_arange() {
+        //testing against np.arange
+        let result = arange(0., 10., 1.);
+        let answer = [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.];
+        for (r, a) in zip(result, answer) {
+            assert_eq!(r, a)
+        }
+
+        let result = arange(0., 0.5, 0.1);
+        let answer = [0., 0.1, 0.2, 0.3, 0.4];
+        for (r, a) in zip(result, answer) {
+            assert!((r - a).abs() < 1e-7)
+        }
+    }
+
+    #[test]
+    fn test_fd() {
+        let data = arange(0., 100., 1.);
+        let result = calculate_fd(data);
+        let answer = 21.616;
+        assert!((result - answer).abs() < 1e-3)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,91 @@
-pub mod cosmology;
 pub mod constants;
+pub mod cosmology;
+pub mod histogram;
 
+use integrate::adaptive_quadrature;
+use libm::log10;
+use rand_distr::{Distribution, Normal};
+use std::f64::consts::PI;
 
+use crate::cosmology::Cosmology;
+
+/// calcuates the redshift at which the current galaxy with magntidue mag and redshift z would be
+/// visible
+fn calculate_max_z(mag: f64, z: f64, mag_lim: f64, cosmo: Cosmology) -> f64 {
+    let distance_measured = cosmo.luminosity_distance(z) * 1e6; // Mpc to pc
+    let max_distance = 10_f64.powf((mag_lim - mag + 5. * log10(distance_measured)) / 5.);
+    cosmo.inverse_lumdist(max_distance)
+}
+
+/// Calculates the density corrected volume based on the given overdensity function delta_z
+fn calculate_v_dc_max<F: Fn(f64) -> f64 + Sync + Copy>(
+    z_max: f64,
+    delta_z: F,
+    cosmo: Cosmology,
+) -> f64 {
+    let integrand = |z: f64| delta_z(z) * cosmo.differential_comoving_distance(z);
+    let tolerance = 1e-5;
+    let min_h = 1e-7;
+    let v_dc =
+        adaptive_quadrature::adaptive_simpson_method(integrand, 0.0, z_max, min_h, tolerance)
+            .expect("Value too close to zero. Must be within 10e-8");
+    v_dc * 4. * PI
+}
+
+/// Reflect the value if it is outside of the min values back within the bounds.
+fn reflect_into_range(value: f64, min_value: f64, max_value: f64) -> f64 {
+    if value < min_value {
+        2. * min_value - value
+    } else if value > max_value {
+        2. * max_value - value
+    } else {
+        value
+    }
+}
+
+/// Create randoms values within
+fn populate_volume(z: f64, z_max: f64, n_points: i32, cosmo: Cosmology) -> Vec<f64> {
+    let mut random_volumes = Vec::new();
+    let volume = cosmo.comoving_volume(z);
+    let max_volume = cosmo.comoving_volume(z_max);
+    let sigma_vol = 3.5e9;
+    let mut min_vol = volume - 2. * sigma_vol;
+    let mut max_vol = volume + 2. * sigma_vol;
+    if min_vol < 0. {
+        min_vol = 0.;
+    }
+    if max_vol > max_volume {
+        max_vol = max_volume;
+    }
+
+    let normal = Normal::new(volume, sigma_vol).unwrap();
+    let mut counter = 0;
+
+    while counter < n_points {
+        let v = normal.sample(&mut rand::rng());
+        if (v < max_vol) && (v > min_vol) {
+            random_volumes.push(v);
+            counter += 1;
+        }
+    }
+    random_volumes
+        .iter()
+        .map(|&v| cosmo.inverse_covol(v))
+        .collect::<Vec<f64>>()
+}
+
+fn approximate_delta(
+    real_redshifts: Vec<f64>,
+    random_redshifts: Vec<f64>,
+    redshift_bins: Vec<f64>,
+    n_clone: i32,
+) {
+    let center_redshift_bins: Vec<f64> = redshift_bins
+        .windows(2)
+        .map(|b| (b[0] + b[1]) / 2.)
+        .collect();
+
+}
 
 fn main() {
     println!("Hello, world!");
